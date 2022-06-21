dbed703c0ffc12015d650dc1fb071bcbd89b583a
James Rather <105876436+jrather@users.noreply.github.com>
2022-06-21 02:12:47 +0000

    tweaks to flow, fixed a bug, need to build out the logic further to account for all of the permutations of a given site plan application

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def input_repl(text = ''):
   print(text, end = '')
   return input()
 
-print("Welcome! Please answer the questions below to determine what type of project you have.")
+print("Welcome! Please answer the questions below to determine what type of planning application you will need to submit. For each question, please input numbers without commas.")
 sf_of_development = int(input("How many square feet of new floor area will your project include? "))
 if sf_of_development > 500 and sf_of_development < 10000:
   print ("OK - lets check a few more things.")
@@ -17,10 +17,10 @@ elif land_disturbance > 1000:
   print("OK - let's check a few more things.")
 elif land_disturbance > 130680:
   print("Major Site Plan review is required for this.")
-change_of_use = int(input("Are you changing the use of your site? If so, how many square feet is this use?" ))
-if change_of_use > 10000:
+change_of_use = int(input("Are you changing the use of your site? If so, how many square feet is this use? "))
+if change_of_use < 10000:
   print("This change of use can be reviewed by Permitting - Planning review is not required.")
-elif change_of_use < 10000 and change_of_use > 20000:
+elif change_of_use > 10000 and change_of_use < 20000:
   print("This change of use requires Minor Site Plan review.")
 elif change_of_use > 20000:
   print("This change of use requires Major Site Plan review.")
